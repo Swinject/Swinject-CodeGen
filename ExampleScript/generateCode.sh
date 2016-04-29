ef697f0db40246c629b6f883595ef4164b6f0920
Wolfgang Lutz <wlut@num42.de>
2016-04-29 09:37:24 +0200

    add conversion examples

--- ExampleScript/generateCode.sh
+++ ExampleScript/generateCode.sh
@@ -1,2 +1,7 @@
 #/bin/sh
 ../swinject_cg -i example.csv -o containerExtension.swift
+../swinject_cg -i example.yml -o containerExtension2.swift
+../swinject_cg -i example.csv  -c
+../swinject_cg -i example.yml  -c
+../swinject_cg -i example.csv.yml -o containerExtension3.swift
+../swinject_cg -i example.yml.csv -o containerExtension4.swift
