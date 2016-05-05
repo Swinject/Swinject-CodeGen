fb4a5f97b090f469c952049a62b805e85f152ebe
Wolfgang Lutz <wlut@num42.de>
2016-05-05 17:37:51 +0200

    Add a simple test, that checks whether generatedCode is equal to expected Code (#8)

---
+++ test.rb
@@ -0,0 +1,39 @@
+require 'minitest/autorun'
+
+class TestCodegeneration < MiniTest::Unit::TestCase
+  def buildCSV(fileName)
+    %x(./swinject_codegen -i #{fileName}).chomp
+  end
+
+  def readFile(fileName)
+    File.read(fileName).chomp
+  end
+
+  def runTest(baseFileName)
+    assert_equal buildCSV("ReadmeExamples/#{baseFileName}.csv"), readFile("ExpectedCode/#{baseFileName}.swift")
+  end
+
+  def test_example_a
+    runTest("ExampleA")
+  end
+
+  def test_example_b
+    runTest("ExampleB")
+  end
+
+  def test_example_c
+    runTest("ExampleC")
+  end
+
+  def test_example_d
+    runTest("ExampleD")
+  end
+
+  def test_example_e
+    runTest("ExampleE")
+  end
+
+  def test_example_f
+    runTest("ExampleF")
+  end
+end
